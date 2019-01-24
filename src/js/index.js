828463bfff67e2c7a0975b7ddd070f9979f9a92e
Dan Chamorro <dan@danchamorro.com>
2019-01-23 19:15:10 -0500

    Added webpack dotenv, succesfully geting data from food api

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
 // Global app controller
-import num from "./test";
-const x = 23;
+import axios from "axios";
 
-console.log(`I imported ${num} from another module. Variable x is ${x}`);
+async function getResults(query) {
+  const key = process.env.API_KEY;
+  try {
+    const res = await axios(
+      `https://www.food2fork.com/api/search?key=${key}&q=${query}`
+    );
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+getResults("pizza");
